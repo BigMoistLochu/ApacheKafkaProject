5d2fbae31c7f31557c6afdf4d725d9d6a3efe024
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-30 20:33:04 +0200

    create config for topicKafka

---
+++ src/test/java/com/example/apachekafkaproject/ApacheKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apachekafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
